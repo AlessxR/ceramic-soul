3c195b08b9e9aa5e0f005a9b3a3921a5ccb10005
AlessxR <alexxsrs.al@gmail.com>
2026-02-20 21:05:10 +0200

    catalog page, tabs, adapt

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-			},
+                catalog: resolve(__dirname, "catalog.html"),
+            },
 		},
 	},
 });
