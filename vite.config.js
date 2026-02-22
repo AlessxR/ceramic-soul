2ef023a41c0d80817766f5db45cc99fab79f4874
AlessxR <alexxsrs.al@gmail.com>
2026-02-22 15:29:12 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import {dirname, resolve} from "node:path";
+import {fileURLToPath} from "node:url";
+import {defineConfig} from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-	build: {
-		rollupOptions: {
-			input: {
-				main: resolve(__dirname, "index.html"),
-                catalog: resolve(__dirname, "catalog.html"),
-                about: resolve(__dirname, "about.html"),
-                blog: resolve(__dirname, "blog.html"),
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                catalog: "catalog.html",
+                about: "about.html",
+                blog: "blog.html",
             },
-		},
-	},
+        },
+    },
 });
